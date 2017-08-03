2b91f1629af3cb44e2486b00a55b3d7cd00c9b76
danielkakitek <d_kakitek@hotmail.com>
2017-08-03 16:17:12 +0200

    add facebook

--- src/main/java/com/example/memy/controller/HelloController.java
+++ src/main/java/com/example/memy/controller/HelloController.java
@@ -4,7 +4,12 @@ package com.example.memy.controller;
 import com.example.memy.DAO.CategoryDao;
 import com.example.memy.DAO.GifDao;
 import com.example.memy.model.category;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
@@ -69,4 +74,28 @@ public class HelloController {
     public String searchByCategory(ModelMap modelMap){
 return "category";
     }
+
+
+    private Facebook facebook;
+    private ConnectionRepository connectionRepository;
+
+    public HelloController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
+    }
+
+    @GetMapping("/facebook")
+    public String helloFacebook(Model model) {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
+        PagedList<Post> feed = facebook.feedOperations().getFeed();
+        model.addAttribute("feed", feed);
+        return "hello";
+    }
+
+
+
 }
